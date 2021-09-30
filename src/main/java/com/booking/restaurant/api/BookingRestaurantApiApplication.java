0cfa7a758e83f9cdf3cc114344516323fabb23c2
marcorebollo1980 <marcorebollolopez@gmail.com>
2021-09-30 14:25:07 -0500

    Se agrega el proyecto base inicial

---
+++ src/main/java/com/booking/restaurant/api/BookingRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.booking.restaurant.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingRestaurantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/restaurant/api/BookingRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.restaurant.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
